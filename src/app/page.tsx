5e970c4cd03b26013cee8b6af736ba384186bcd7
Akshayak603 <akshaykumar.ak603@gmail.com>
2024-04-17 22:02:46 +0530

    Header and Footer added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Section1 from "@/components/Section1";
 import Section2 from "@/components/Section2";
 import Section3 from "@/components/Section3";
 import Section4 from "@/components/Section4";
+import Footer from "@/components/Footer"
 import Image from "next/image";
 
 export default function Home() {
@@ -13,6 +14,7 @@ export default function Home() {
       <Section2 />
       <Section3 />
       <Section4 />
+      <Footer />
     </main>
   );
 }
